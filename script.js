d812c43a8ba2e97177f1f606e0532039455f850f
Nek-Codes <ekenhibo@gmail.com>
2026-09-21 23:03:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-const sheet = document.getElementById("sheet1");
+```javascript
+const pages = [
+    "Pages/Bi-monthly Bulletin.png",
+    "Pages/contents.png"
+];
 
-let flipped = false;
+const pageContainer = document.getElementById("pageContainer");
+const nextPage = document.getElementById("nextPage");
+const prevPage = document.getElementById("prevPage");
 
-sheet.addEventListener("click", function () {
+let currentPage = 0;
 
-    flipped = !flipped;
 
-    if (flipped) {
-        sheet.classList.add("flipped");
-    } else {
-        sheet.classList.remove("flipped");
+/* Create the pages */
+
+pages.forEach((image, index) => {
+
+    const sheet = document.createElement("div");
+
+    sheet.className = "sheet";
+
+    sheet.style.zIndex = pages.length - index;
+
+    sheet.innerHTML = `
+        <div class="page">
+            <img src="${image}" alt="Bulletin page ${index + 1}">
+        </div>
+    `;
+
+    pageContainer.appendChild(sheet);
+});
+
+
+const sheets = document.querySelectorAll(".sheet");
+
+
+/* NEXT */
+
+nextPage.addEventListener("click", () => {
+
+    if (currentPage < sheets.length - 1) {
+
+        sheets[currentPage].classList.add("flipped");
+
+        currentPage++;
     }
 
 });
+
+
+/* PREVIOUS */
+
+prevPage.addEventListener("click", () => {
+
+    if (currentPage > 0) {
+
+        currentPage--;
+
+        sheets[currentPage].classList.remove("flipped");
+    }
+
+});
+```
